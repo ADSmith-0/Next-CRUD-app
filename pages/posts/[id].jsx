d40d366e4d0e0d29931156f68545083d2eb20e96
ADSmith-0 <adam00199382818@gmail.com>
2023-04-16 15:57:39 +0100

    added: individual pages for each post and refactored: api routes

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,10 +1,12 @@
+import { Button, Typography } from "@mui/material";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useRouter } from "next/router";
+
 // could be getStaticProps, depending on how frequently
 export async function getServerSideProps(context){
     const { id } = context.params;
-    console.log(id);
-    // const response = await fetch(`http://localhost:3000/api/get-post/${id}`);
-    // const data = await response.json();
-    const data = {"hello": "world"};
+    const response = await fetch(`http://localhost:3000/api/get-posts/${id}`);
+    const data = await response.json();
     return {
         props: {
             postData: data
@@ -13,8 +15,23 @@ export async function getServerSideProps(context){
 }
 
 export default function Post({ postData }) {
-    console.log(postData);
+    const { title, body } = postData;
+    const router = useRouter();
+
+    const handleClick = e => {
+        router.push("/posts");
+    }
+
     return (
-        <div>post</div>
+        <>
+            {/* Ideally this would be a link but idk how to style it like MUI button as a link */}
+            <Button startIcon={<ArrowBackIcon />} onClick={handleClick}>Posts</Button>
+            <Typography variant="h1" gutterBottom sx={{ fontSize: "4em" }}>
+                {title}
+            </Typography>
+            <Typography variant="body1" color="text.secondary" gutterBottom>
+                {body}
+            </Typography>
+        </>
     )
 }
